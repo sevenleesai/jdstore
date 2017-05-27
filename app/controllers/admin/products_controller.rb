1eb79e1452f19a111bbe89eebf202f8b71289e2e
陈小敏 <chenxiaomin@chenxiaomins-MacBook-Pro.local>
2017-05-27 12:50:30 +0800

    basic category

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -10,14 +10,17 @@ class Admin::ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    @categories = Category.all.map { |c| [c.name, c.id] }
   end
 
   def edit
     @product = Product.find(params[:id])
+    @categories = Category.all.map { |c| [c.name, c.id] }
   end
 
   def update
     @product = Product.find(params[:id])
+    @product.category_id = params[:category_id]
 
     if @product.update(product_params)
       redirect_to admin_products_path
@@ -28,6 +31,7 @@ class Admin::ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+    @product.category_id = params[:category_id]
 
     if @product.save
       redirect_to admin_products_path
@@ -51,6 +55,6 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price, :image)
+    params.require(:product).permit(:title, :description, :quantity, :price, :image, :category_id)
   end
 end
